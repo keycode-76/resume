63ec58ba50c12a095fa469d135c0910fa772eaa3
unknown <ormomax@gmail.com>
2026-03-11 19:21:09 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ export default defineConfig({
     host: '0.0.0.0', // 允許外部訪問
     port: 5173,
     strictPort: false, // 如果端口被占用，嘗試下一個可用端口
+  },
+  publicDir: 'assets', // 將 assets 目錄作為 public 目錄，直接複製到 dist，不進行處理
+  build: {
+    copyPublicDir: true, // 確保複製 public 目錄
   }
 })
